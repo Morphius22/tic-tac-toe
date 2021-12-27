f947369f12feeaf9a674528331e5f3f7801e8d6e
Eric G <peaceandhealth@protonmail.com>
2021-12-27 16:13:43 -0500

    updated displayBoard to only display squares without symbols

--- logic.js
+++ logic.js
@@ -4,32 +4,61 @@ const Player = (name, symbol) => {
     const getSymbol = () => symbol;
 
     return {
-        move,
-
+        getName,
+        getSymbol,
     }
 }
 
 const gameBoard = (() => {
 
     const getBoard = () => {
-        let board = ['x','o','x','o','x','o','x','o','x']
+        let board = ['x','o','x','o','x','o','x','o','x'];
         return board;
-    }
+    };
 
-    const displayBoard = (board) => {
-        board.forEach(position => {
-            const boardGrid = document.querySelector(".board")
-            const placeSymbol = document.createElement('p');
-            placeSymbol.textContent = position;
-            placeSymbol.classList.add('symbol');
-            boardGrid.appendChild(placeSymbol);
-        })
-    }
+    const displayBoard = () => {
+
+        for (let i = 0; i < 9; i++) {
+            const boardGrid = document.querySelector(".board");
+            const placeDiv = document.createElement('div');
+            boardGrid.appendChild(placeDiv);
+            placeDiv.classList.add('square')
+        };
+    };
+
+    // const displaySymbols = (board) => {
+    //     boardDivs = document.querySelectorAll('.square');
+    //     boardDivs.forEach((div, i) => {
+    //         //if the current loop matches the data attribute of the array position, then add symbol. If not, continue.
+    //         if () {
+    //             //place symbol into the div
+    //             const placeSymbol = document.createElement('p');
+    //             placeSymbol.textContent = board.index;
+    //             placeSymbol.classList.add('symbol');
+    //             div.appendChild(placeSymbol);
+    //         } else () {
+    //             continue
+    //         }
+    //     })
+    // }
 
     return {
         displayBoard,
-        getBoard
+        getBoard,
+        // displaySymbols,
     }
 })();
 
-gameBoard.displayBoard(gameBoard.getBoard());
+
+gameBoard.displayBoard();
+
+
+//board is generated without any positions in it (done)
+
+//event listeners are set up on each board position
+
+//when an event listener is clicked, the position number 
+//is added to the array as a new object with a data attribute
+//so I can knw which position was clicked (can i simplify this with event delegation?)
+
+//the new item in the array causes a symbol to be addded into the clicked div
